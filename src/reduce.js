cbee0adcc87d9d843ef3730ebbf7f524f3c54bac
Szabolcs Sinka <sasi.sinka@icloud.com>
2018-02-17 23:02:16 +0100

    Rename helper -> _reduce.

--- src/reduce.js
+++ src/reduce.js
@@ -1,7 +1,7 @@
-const helper = (f, a, [h, ...t]) => h != undefined
+const _reduce = (f, a, [h, ...t]) => h != undefined
   ? helper(f, f(a, h), t)
   : a;
 
 const reduce = (f, [h, ...t], i) => i == undefined
   ? !t.length ? h : reduce(f, t, h)
-  : helper(f, i, [h, ...t]);
+  : _reduce(f, i, [h, ...t]);
