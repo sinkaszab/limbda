49e893148be8b22ce2cb0e4c4e4a982b757bd70b
Szabolcs Sinka <sasi.sinka@icloud.com>
2018-03-06 22:36:10 +0100

    Limbda export isIterable.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
+const isIterable = require('./lib/isIterable.js');
 const reduce = require('./lib/reduce.js');
 const map = require('./lib/map.js');
 const filter = require('./lib/filter.js');
 
-const limbda = { reduce, map, filter };
+const limbda = { isIterable, reduce, map, filter };
 
 module.exports = limbda;
-
