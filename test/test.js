42fe2b419dd62dded4fe17020903abc59e49b95a
Szabolcs Sinka <sasi.sinka@icloud.com>
2018-04-20 10:55:33 +0200

    Add first test case for reduce.

--- test/test.js
+++ test/test.js
@@ -1,1 +1,8 @@
-var test = require('tape');
+const test = require('tape');
+const { reduce } = require('./../index.js');
+
+test('Reduce to a value with initial value set.', function (t) {
+  t.plan(1);
+  t.equal(reduce((a, v) => a + v, [1, 2, 3, 4, 5], 0), 15);
+  t.end();
+});
