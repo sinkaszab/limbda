2b574323dc9cba2b43c25faeeb184849ccef9c39
Szabolcs Sinka <sasi.sinka@icloud.com>
2018-04-21 00:19:48 +0200

    Improve reduce's return value per parameter variation & extend test cases to illustrate them.

--- test/test.js
+++ test/test.js
@@ -1,8 +1,38 @@
 const test = require('tape');
 const { reduce } = require('./../index.js');
 
-test('Reduce to a value with initial value set.', function (t) {
-  t.plan(1);
-  t.equal(reduce((a, v) => a + v, [1, 2, 3, 4, 5], 0), 15);
+// TODO: do tests with all kind of collections. Test on strings as well!
+
+test('Reduce an empty collection.', function (t) {
+  t.plan(4);
+  t.equal(
+    reduce((a, v) => a + v, []),
+    undefined, "Empty collection without initial value returns undefined."
+  );
+  t.equal(
+    reduce((a, v) => a + v, [], 42),
+    42, "Empty collection with initial value returns initial value."
+  );
+  t.equal(
+    reduce((a, v) => a + v, undefined),
+    undefined, "Collection == undefined returns undefined|null, aka coll itself."
+  );
+  t.equal(
+    reduce((a, v) => a + v, null, 12),
+    12, "If collection undefined, but has initial value, that becomes its return value."
+  );
+  t.end();
+});
+
+test('Reduce a non-empty collection.', function (t) {
+  t.plan(2);
+  t.equal(
+    reduce((a, v) => a + v, [21]),
+    21, "A collection with one element will return that one without transformation applied to it."
+  );
+  t.equal(
+    reduce((a, v) => a + v, [1, 2, 3, 4, 5], 0),
+    15, "Reduces to appropriate value."
+  );
   t.end();
 });
