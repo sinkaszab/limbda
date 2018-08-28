36846410a82a692c124d6e85ba688a2b517729ed
Szabolcs Sinka <sasi.sinka@icloud.com>
2018-08-28 10:36:44 +0200

    Start converting draft to TDD & check numeric args.

--- test/04.range.test.js
+++ test/04.range.test.js
@@ -2,12 +2,32 @@ import test from 'tape';
 import { range } from './../lib/limbda';
 
 test(
-  'range | Test draft:',
-  function testRequiredArguments(t) {
-    t.plan(1);
-    t.deepEquals( // 1
-      [...range(5, 9)],
-      [5, 6, 7, 8, 9], 'Range',
+  'range | Args start, end & step throw when any of them is NaN:',
+  function throwIfStartEndStepIsNaN(t) {
+    const argIsNaNException = new RegExp('Start, end, step arguments cannot be NaN.');
+    t.plan(3);
+    t.throws( // 1
+      () => [...range('a')],
+      argIsNaNException, '"start" cannot be NaN.'
+    );
+    t.throws( // 2
+      () => [...range(1, 'a')],
+      argIsNaNException, '"end" cannot be NaN.'
+    );
+    t.throws( // 2
+      () => [...range(1, 5, 'a')],
+      argIsNaNException, '"step" cannot be NaN.'
     );
   },
 );
+
+// test(
+//   'range | Test draft:',
+//   function testRequiredArguments(t) {
+//     t.plan(1);
+//     t.deepEquals( // 1
+//       [...range(5, 9)],
+//       [5, 6, 7, 8, 9], 'Range',
+//     );
+//   },
+// );
