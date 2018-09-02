0598424175dff4b07cbb27c75529344579954d09
Szabolcs Sinka <sasi.sinka@icloud.com>
2018-09-02 12:22:17 +0200

    Validate arguments with test cases.

--- test/04.range.test.js
+++ test/04.range.test.js
@@ -8,15 +8,41 @@ test(
     t.plan(3);
     t.throws( // 1
       () => [...range('a')],
-      argIsNaNException, '"start" cannot be NaN.'
+      argIsNaNException, '"start" cannot be NaN.',
     );
     t.throws( // 2
       () => [...range(1, 'a')],
-      argIsNaNException, '"end" cannot be NaN.'
+      argIsNaNException, '"end" cannot be NaN.',
+    );
+    t.throws( // 3
+      () => [...range(1, 5, 'a')],
+      argIsNaNException, '"step" cannot be NaN.',
+    );
+  },
+);
+
+test(
+  'range | the from "start" to "end" set must contain series:',
+  function throwIfBoundary(t) {
+    const endLessThanStartException = new RegExp('End is less than start, while series is increasing.');
+    const endGreaterThanStartException = new RegExp('End is greater than start, while series is decreasing.');
+    const seriesIncluded = 'Doesn\'t throw if series is contained by "start-end" interval.';
+    t.plan(4);
+    t.throws( // 1
+      () => [...range(5, 2, +2)],
+      endLessThanStartException, '"end" must be greater than start when series is increasing.',
     );
     t.throws( // 2
-      () => [...range(1, 5, 'a')],
-      argIsNaNException, '"step" cannot be NaN.'
+      () => [...range(1, 10, -2.2)],
+      endGreaterThanStartException, '"end" must be less than start when series is increasing.',
+    );
+    t.doesNotThrow( // 3
+      () => [...range(5, -10, -3)],
+      endLessThanStartException, seriesIncluded,
+    );
+    t.doesNotThrow( // 4
+      () => [...range(1, 10, 3)],
+      endGreaterThanStartException, seriesIncluded,
     );
   },
 );
