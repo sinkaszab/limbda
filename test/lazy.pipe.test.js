27c702a717e2857705c630b9af630732d8611ff4
Szabolcs Sinka <sasi.sinka@icloud.com>
2019-02-12 00:54:15 +0100

    Create basic test for `lazy.pipe`.
    `lazy.pipe` now works as expected
    with refactored eager `pipe` fn.

--- test/lazy.pipe.test.js
+++ test/lazy.pipe.test.js
@@ -1,11 +1,14 @@
-// import test from 'tape';
+import test from 'tape';
 import { lazy } from '../dist/limbda';
 
 const { pipe } = lazy;
 
-const inc = (v) => v + 1;
-const dup = (v) => v * 2;
-
-const lazyPiped = pipe(inc, dup);
-
-console.log([...lazyPiped([20, 40, 80])]);
+test('lazy pipe | composes from left to right', (t) => {
+  const inc = (v) => v + 1;
+  const dup = (v) => v * 2;
+  t.plan(1);
+  t.deepEquals(
+    [...(pipe(inc, dup)([20, 40, 80]))],
+    [42, 82, 162],
+  );
+});
