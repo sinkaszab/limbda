bfa08dd0dc9a1bf16b7e6ff70ba5a288b18d4ddc
Szabolcs Sinka <sasi.sinka@icloud.com>
2019-02-11 22:18:49 +0100

    Create basic tests for lazy.ObjectEntries.

--- test/lazy.ObjectEntries.test.js
+++ test/lazy.ObjectEntries.test.js
@@ -4,9 +4,9 @@ import { lazy } from '../dist/limbda';
 const { ObjectEntries } = lazy;
 
 test(
-  'lazy.ObjectEntries | Exhausted generated result is equivalent to Object.entries.',
+  'lazy.ObjectEntries | Exhausted generated result',
   function iterate(t) {
-    t.plan(1);
+    t.plan(3);
     const simpleObj = { a: 'a', b: 'b' };
     const generatedEntries = [];
     /* eslint-disable no-restricted-syntax */
@@ -17,7 +17,29 @@ test(
     t.deepEquals(
       generatedEntries,
       Object.entries(simpleObj),
-      'for Object literal'
+      'is equivalent to Object.entries'
+    );
+
+    const objectWithProto = Object.create(simpleObj);
+    objectWithProto.c = 'c';
+    objectWithProto.d = 'd';
+
+    const generatedEntriesOfObjectWithProto = [];
+    /* eslint-disable no-restricted-syntax */
+    for (const entry of ObjectEntries(objectWithProto)) {
+      generatedEntriesOfObjectWithProto.push(entry);
+    }
+    /* eslint-enable no-restricted-syntax */
+
+    t.deepEquals(
+      generatedEntriesOfObjectWithProto,
+      Object.entries(objectWithProto),
+      'lists only Objects\'s own properties (#1)',
+    );
+    t.deepEquals(
+      generatedEntriesOfObjectWithProto,
+      [['c', 'c'], ['d', 'd']],
+      'lists only Objects\'s own properties (#2)',
     );
   },
 );
