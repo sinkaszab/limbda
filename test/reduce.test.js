b5fff65c3194fa240fd8b192f3de840d6d1a0097
Szabolcs Sinka <sasi.sinka@icloud.com>
2019-02-24 17:27:47 +0100

    Purge outdated comment from reduce tests.

--- test/reduce.test.js
+++ test/reduce.test.js
@@ -1,10 +1,6 @@
 import test from 'tape';
 import { reduce } from '../dist/limbda';
 
-// TODO: create a test that checks for max call-stack size exceeded.
-//       - Recursion is not optimized in engines.
-//       - Follow-up: implement for all limbda fns.
-
 test(
   'reduce | Required arguments:',
   function testRequiredArguments(t) {
